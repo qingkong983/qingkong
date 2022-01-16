320fd461f04b9105ac57ab02397fbfef358b98b9
Allen Zhang <wr_zhang@163.com>
2022-01-17 00:32:02 +0800

    feat: upd

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './assets/css/index.css'
 import App from './App'
+import 'antd/dist/antd.min.css'
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HashRouter>
+        <App />
+    </HashRouter>,
+    document.getElementById('root')
 )
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'root-entry-name': 'default'
+        }
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: /^~/, replacement: '' },
+    ],
+  },
 })
