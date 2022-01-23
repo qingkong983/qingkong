e84903ac2359e26f9db589f49c3e9531e5ecd478
Allen Zhang <wr_zhang@163.com>
2022-01-23 22:55:47 +0800

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './apps/user/user.module'
-import { CalendarModule } from './apps/calendar/calendar.module';
-
+import { CalendarModule } from './apps/calendar/calendar.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 @Module({
-  imports: [UserModule, CalendarModule],
+  imports: [
+    UserModule,
+    CalendarModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
