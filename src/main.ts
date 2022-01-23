9c1da1b48956fc15a84ae08f0055e8f09425f980
Allen Zhang <wr_zhang@163.com>
2022-01-23 22:52:53 +0800

    upd

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ async function bootstrap() {
   prepareInit()
   const { AppModule } = await import('./app.module')
   const app = await NestFactory.create(AppModule)
+  app.use(function (req, res, next) {
+    const old_url = req.url
+    if (old_url.includes('/api/cov')) {
+      req.url = old_url.replace('/api/cov', '')
+      console.log('foo: ' + old_url + ' -> ' + req.url)
+    }
+    next()
+  })
   app.enableCors()
   await app.listen(8080)
 }
