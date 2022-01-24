019f5cadd8b6eac89db2c563ba0bce5c3d829471
Allen Zhang (张涛) <tzhangm@trip.com>
2022-01-24 20:17:27 +0800

    upd5

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { prepareInit } from './utils/prepareInit'
+import * as compression from 'compression'
 
 async function bootstrap() {
   prepareInit()
   const { AppModule } = await import('./app.module')
   const app = await NestFactory.create(AppModule)
+  app.use(compression())
   app.use(function (req, res, next) {
     const old_url = req.url
     if (old_url.includes('/api/cov')) {
