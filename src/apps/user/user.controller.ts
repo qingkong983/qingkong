76bd094059161d4cb995c3ca18fd6ed19598c5d6
张涛 <tzhangm@trip.com>
2022-08-21 18:17:09 +0800

    添加日志

--- src/apps/user/user.controller.ts
+++ src/apps/user/user.controller.ts
@@ -8,10 +8,10 @@ import {
   Delete,
   UseGuards,
   Request,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import axios from 'axios';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import axios from 'axios'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 
 @Controller('user')
 export class UserController {
@@ -25,61 +25,60 @@ export class UserController {
     if (createUserDto.code.length < 8) {
       // 2.拿openId查是否注册了，注册了在内部模拟登陆，拿token
       const checkIsHasUserRes = await this.userService.checkIsHasUser(
-          createUserDto.code,
-      );
+        createUserDto.code,
+      )
       return {
         token: checkIsHasUserRes.token,
         openid: createUserDto.code,
         session_key: 'session_key',
         isHasUser: true,
-      };
+      }
     }
 
     // 1.拿code查openid
     const jscode2sessionRes = await axios
-        .get(`https://api.weixin.qq.com/sns/jscode2session`, {
-          params: {
-            appid: 'wx658151ab012a8e06',
-            secret: '1525008cc7c17ec8300d985e6f5939a8',
-            js_code: createUserDto.code,
-            grant_type: 'authorization_code',
-          },
-        })
-        .then((res) => res.data);
+      .get(`https://api.weixin.qq.com/sns/jscode2session`, {
+        params: {
+          appid: 'wx658151ab012a8e06',
+          secret: '1525008cc7c17ec8300d985e6f5939a8',
+          js_code: createUserDto.code,
+          grant_type: 'authorization_code',
+        },
+      })
+      .then((res) => res.data)
     // 2.拿openId查是否注册了，注册了在内部模拟登陆，拿token
     const checkIsHasUserRes = await this.userService.checkIsHasUser(
-        jscode2sessionRes.openid,
-    );
+      jscode2sessionRes.openid,
+    )
     // 返回数据
     return {
       token: checkIsHasUserRes.token,
       openid: jscode2sessionRes.openid,
       session_key: jscode2sessionRes.session_key,
       isHasUser: checkIsHasUserRes.token ? true : false,
-    };
+    }
   }
 
   @Post()
   create(@Body() createUserDto: any) {
-    return this.userService.create(createUserDto);
+    return this.userService.create(createUserDto)
   }
 
   @Get()
   findAll() {
-    return this.userService.findAll();
+    return this.userService.findAll()
   }
 
   // 需要鉴权的接口
   @UseGuards(JwtAuthGuard)
   @Get('getuserinfo')
   findOne(@Request() request: { user: { id: number } }) {
-    return this.userService.findOne(request.user.id);
+    return this.userService.findOne(request.user.id)
   }
 
   @Post('wxCreateAndUpdate')
   wxCreateAndUpdate(@Body() body) {
-    console.log(body, 'body');
-    return this.userService.wxCreateAndUpdate(body);
+    return this.userService.wxCreateAndUpdate(body)
   }
 
   // @Delete(':id')
--- src/apps/user/user.service.ts
+++ src/apps/user/user.service.ts
@@ -1,46 +1,45 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-import axios from 'axios';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+import axios from 'axios'
 
 @Injectable()
 export class UserService {
   constructor(
-      @Inject('user_REPOSITORY')
-      private userRepository: Repository<User>,
+    @Inject('user_REPOSITORY')
+    private userRepository: Repository<User>,
   ) {}
   create(createUserDto: any) {
-    return 'This action adds a new user';
+    return 'This action adds a new user'
   }
 
   findAll() {
-    return `This action returns all user`;
+    return `This action returns all user`
   }
 
   findOne(id: number) {
-    return this.userRepository.findOne({ id });
+    return this.userRepository.findOne({ id })
   }
 
   // 新建用户或者更新
   async wxCreateAndUpdate(params) {
-    const userInfo = params.userInfo;
-    const weappId = params.weappId;
+    const userInfo = params.userInfo
+    const weappId = params.weappId
 
     const findOneByWeappIdRes = await this.userRepository.findOne({
       weappId: weappId,
-    });
-    console.log(findOneByWeappIdRes,'findOneByWeappIdRes')
+    })
 
     if (findOneByWeappIdRes) {
       return this.userRepository.update(
-          { weappId: weappId },
-          {
-            gender: userInfo.gender,
-            nickName: userInfo.nickName,
-            avatarUrl: userInfo.avatarUrl,
-            city: userInfo.city,
-          },
-      );
+        { weappId: weappId },
+        {
+          gender: userInfo.gender,
+          nickName: userInfo.nickName,
+          avatarUrl: userInfo.avatarUrl,
+          city: userInfo.city,
+        },
+      )
     } else {
       const insertRes = await this.userRepository.insert([
         {
@@ -49,47 +48,55 @@ export class UserService {
           nickName: userInfo.nickName,
           avatarUrl: userInfo.avatarUrl,
           city: userInfo.city,
-          username:weappId,
-          password:weappId
+          username: weappId,
+          password: weappId,
         },
-      ]);
-      return insertRes;
+      ])
+      return insertRes
     }
   }
 
   async checkIsHasUser(weappId) {
-    console.log(weappId,'weappId')
-    const findWeappIdRes = await this.userRepository.findOne({ weappId });
+    const findWeappIdRes = await this.userRepository.findOne({ weappId })
     if (findWeappIdRes) {
       const authLoginRes = await axios
-          .post('http://127.0.0.1:8080/auth/login', {
-            username: findWeappIdRes.username,
-            password: findWeappIdRes.password,
-          })
-          .then((res) => res.data);
+        .post('http://127.0.0.1:8080/auth/login', {
+          username: findWeappIdRes.username,
+          password: findWeappIdRes.password,
+        })
+        .then((res) => res.data)
+      axios
+        .post('https://www.api.rico.org.cn/purplelog/log', {
+          tags: ['weapp-api'],
+          level: 'info',
+          msg: JSON.stringify(authLoginRes),
+        })
+        .then((r) => {
+          console.log(r)
+        })
       return {
         token: authLoginRes.access_token,
-      };
+      }
     } else {
       return {
         token: null,
-      };
+      }
     }
     // return `This action removes a #${weappId} user`;
   }
 
   async passwordLogin({ username, password }) {
-    const user = await this.userRepository.find({ username, password });
+    const user = await this.userRepository.find({ username, password })
     if (user.length > 0) {
-      return user[0];
+      return user[0]
     } else {
       throw new HttpException(
-          {
-            statusCode: HttpStatus.FORBIDDEN,
-            message: '账号密码错误',
-          },
-          HttpStatus.FORBIDDEN,
-      );
+        {
+          statusCode: HttpStatus.FORBIDDEN,
+          message: '账号密码错误',
+        },
+        HttpStatus.FORBIDDEN,
+      )
     }
   }
 }
